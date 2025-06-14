db0961d4db4770a76d60dea1e95190a926a610c1
pillared <anthonyasilo273@gmail.com>
2025-06-14 01:33:17 +0000

    sick

--- app/layout-provider.tsx
+++ app/layout-provider.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Header from "@/components/header";
+import AnimatedIconsBackground from "@/components/animated-icons-background";
 
 interface LayoutProviderProps {
   children: ReactNode;
@@ -8,6 +9,7 @@ interface LayoutProviderProps {
 export default function LayoutProvider({ children }: LayoutProviderProps) {
   return (
     <>
+      <AnimatedIconsBackground />
       <Header />
       {children}
     </>
