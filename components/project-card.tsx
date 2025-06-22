584f7c38f581e3308555db8511f44f8fd4e02ab1
pillared <anthonyasilo273@gmail.com>
2025-06-22 03:13:09 +0000

    colors, configs,

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -30,7 +30,7 @@ const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
     <Card className="z-0 mx-auto my-6 max-w-lg border border-blue-200 bg-gradient-to-br from-sky-50 to-blue-100 shadow-md transition-all duration-200 hover:shadow-lg dark:border-zinc-700 dark:from-zinc-800 dark:to-gray-900">
       <CardHeader>
         <CardTitle className="flex items-center gap-2 text-lg text-blue-900 dark:text-blue-200">
-          <Sparkles className="h-5 w-5 text-blue-600 dark:text-blue-400" />
+          {/* <Sparkles className="h-5 w-5 text-blue-600 dark:text-blue-400" /> */}
           {project.name}
         </CardTitle>
         <CardDescription className="text-sm text-gray-500 dark:text-gray-400">
@@ -38,8 +38,10 @@ const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
         </CardDescription>
       </CardHeader>
 
-      <CardContent className="space-y-4 text-gray-800 dark:text-gray-200">
-        <p>{project.description}</p>
+      <CardContent className="space-y-4 text-gray-800 dark:text-gray-400">
+        <p className="rounded-md py-2 transition-colors hover:bg-slate-50 dark:hover:bg-slate-800">
+          {project.description}
+        </p>
 
         <div className="space-y-2">
           {/* <div className="flex items-start gap-2">
@@ -47,29 +49,32 @@ const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
             <p></p>
           </div> */}
 
-          <div className="flex items-start gap-2 rounded-md p-2 transition-colors hover:bg-orange-50">
-            <Code2 className="mt-0.5 h-5 w-5 flex-shrink-0 text-indigo-500" />
-            <div className="text-base text-zinc-800">
+          <div className="flex items-start gap-2 rounded-md p-2 transition-colors hover:bg-slate-50 dark:hover:bg-slate-800">
+            {/* <Code2 className="mt-0.5 h-5 w-5 flex-shrink-0 text-indigo-500" /> */}
+            <div className="text-base text-zinc-800 dark:text-zinc-400">
               <p>
-                <strong>Technologies:</strong> {project.technologies.join(", ")}
+                <strong className="dark:text-blue-100">Technologies:</strong>{" "}
+                {project.technologies.join(", ")}
               </p>
             </div>
           </div>
 
-          <div className="flex items-start gap-2 rounded-md p-2 transition-colors hover:bg-orange-50">
-            <Hammer className="mt-1 h-5 w-5 flex-shrink-0 text-orange-500" />
-            <div className="text-base text-zinc-800">
+          <div className="flex items-start gap-2 rounded-md p-2 transition-colors hover:bg-slate-50 dark:hover:bg-slate-800">
+            {/* <Hammer className="mt-1 h-5 w-5 flex-shrink-0 text-orange-500" /> */}
+            <div className="text-base text-zinc-800 dark:text-zinc-400">
               <p>
-                <strong>Role:</strong> {project.role}
+                <strong className="dark:text-blue-100">Role:</strong>{" "}
+                {project.role}
               </p>
             </div>
           </div>
 
-          <div className="flex items-start gap-2 rounded-md p-2 transition-colors hover:bg-orange-50">
-            <Lightbulb className="mt-1 h-5 w-5 flex-shrink-0 text-yellow-500" />
-            <div className="text-base text-zinc-800">
+          <div className="flex items-start gap-2 rounded-md p-2 transition-colors hover:bg-slate-50 dark:hover:bg-slate-800">
+            {/* <Lightbulb className="mt-1 h-5 w-5 flex-shrink-0 text-yellow-500" /> */}
+            <div className="text-base text-zinc-800 dark:text-zinc-400">
               <p>
-                <strong>Learnings:</strong> {project.learnings}
+                <strong className="dark:text-blue-100">Learnings:</strong>{" "}
+                {project.learnings}
               </p>
             </div>
           </div>
@@ -78,11 +83,12 @@ const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
             <p></p>
           </div>
 
-          <div className="flex items-start gap-2 rounded-md p-2 transition-colors hover:bg-orange-50">
-            <BadgeCheck className="mt-1 h-5 w-5 flex-shrink-0 text-green-500" />
-            <div className="text-base text-zinc-800">
+          <div className="flex items-start gap-2 rounded-md p-2 transition-colors hover:bg-slate-50 dark:hover:bg-slate-800">
+            {/* <BadgeCheck className="mt-1 h-5 w-5 flex-shrink-0 text-green-500" /> */}
+            <div className="text-base text-zinc-800 dark:text-zinc-400">
               <p>
-                <strong>Impact:</strong> {project.impact}
+                <strong className="dark:text-blue-100">Impact:</strong>{" "}
+                {project.impact}
               </p>
             </div>
           </div>
@@ -111,7 +117,7 @@ const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
             href={project.demoLink}
             target="_blank"
             rel="noopener noreferrer"
-            className="rounded-full bg-gray-300 px-4 py-1 text-sm text-blue-700 hover:bg-green-100 active:bg-green-200 dark:text-blue-400"
+            className="rounded-full bg-gray-300 px-4 py-1 text-sm text-blue-700 hover:bg-green-100 active:bg-green-200 dark:bg-gray-700 dark:text-blue-400 dark:hover:bg-slate-800 dark:active:bg-green-200"
           >
             Live Demo
           </a>
@@ -121,7 +127,7 @@ const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
             href={project.repoLink}
             target="_blank"
             rel="noopener noreferrer"
-            className="rounded-full bg-gray-300 px-4 py-1 text-sm text-blue-700 hover:bg-green-100 active:bg-green-200 dark:text-blue-400"
+            className="rounded-full bg-gray-300 px-4 py-1 text-sm text-blue-700 hover:bg-green-100 active:bg-green-200 dark:bg-gray-700 dark:text-blue-400 dark:hover:bg-slate-800 dark:active:bg-green-200"
           >
             Source Code
           </a>
@@ -131,7 +137,7 @@ const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
             href={project.blogPost}
             target="_blank"
             rel="noopener noreferrer"
-            className="rounded-full bg-gray-300 px-4 py-1 text-sm text-blue-700 hover:bg-green-100 active:bg-green-200 dark:text-blue-400"
+            className="rounded-full bg-gray-300 px-4 py-1 text-sm text-blue-700 hover:bg-green-100 active:bg-green-200 dark:bg-gray-700 dark:text-blue-400 dark:hover:bg-slate-800 dark:active:bg-green-200"
           >
             Blog Post
           </a>
